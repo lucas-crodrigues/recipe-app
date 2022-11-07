b2139d6b569a5a2f0ede6b8f65af82c28f09f920
lucascr <lcrodrigues1995@gmail.com>
2022-11-07 14:35:54 -0300

    Add precision and scale to decimal values

--- db/migrate/20221107173301_create_recipes.rb
+++ db/migrate/20221107173301_create_recipes.rb
@@ -2,8 +2,8 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.decimal :preparation_time
-      t.decimal :cooking_time
+      t.decimal :preparation_time, :precision => 2, :scale => 1
+      t.decimal :cooking_time, :precision => 2, :scale => 1
       t.text :description
       t.boolean :public
       t.references :user, null: false, foreign_key: true
