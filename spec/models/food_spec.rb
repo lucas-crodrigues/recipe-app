282cc7c42c1c967a5f930c098473fe2feb1dde86
lucascr <lcrodrigues1995@gmail.com>
2022-11-11 10:37:53 -0300

    Add tests for food

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Food.new(name: 'Tomato', price: 7.99, measurement_unit: 'Kg', user_id: 1) }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'measurement_unit should be present' do
+    subject.measurement_unit = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'user_id must be a float greater or equal than zero' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Price must be a float greater or equal than zero' do
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end
 end
