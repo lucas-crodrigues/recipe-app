c127427ea535fc36bee6dc12bfff1ba5dda7381e
lucascr <lcrodrigues1995@gmail.com>
2022-11-11 10:38:04 -0300

    Add tests for recipe

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Recipe.new(name: 'Rice', preparation_time: 20, cooking_time: 30, description: 'Boiled rice', public: true,
+               user_id: 1)
+  end
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'description should be present' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'user_id must be a float greater or equal than zero' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Cooking_time must be a float greater or equal than zero' do
+    subject.cooking_time = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Preparation_time must be a float greater or equal than zero' do
+    subject.preparation_time = nil
+    expect(subject).to_not be_valid
+  end
 end
