6a57d63718923673ece3e3f42fc80acdd986147d
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-11 20:52:09 -0300

    Create tests for food and recipe models

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(name: 'Fernando', email: 'fernando.silvabr66@gmail.com', password: '123456', password_confirmation: '123456')
+    @food = Food.new(name: 'Test Food', measurement_unit: 'Kg', price: 10, user: @user)
+  end
+
+  it 'name should be present' do
+    @user.name = nil
+    expect(@user).to_not be_valid
+  end
+
+  it 'measurement_unit should be present' do
+    @food.measurement_unit = nil
+    expect(@food).to_not be_valid
+  end
+
+  it 'price should be equal to or greater that 1' do
+    @food.price = 1
+    expect(@food).to be_valid
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(name: 'Fernando', email: 'fernando.silvabr66@gmail.com', password: '123456', password_confirmation: '123456')
+    @recipe = Recipe.new(name: 'Recipe-1', preparation_time: 1, cooking_time: 1, description: 'Test-1', public: true,
+                         user: @user)
+  end
+
+  it 'name should be present' do
+    @user.name = nil
+    expect(@user).to_not be_valid
+  end
+
+  it 'preparation_time should be present' do
+    @recipe.preparation_time = -1
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'cooking_time should be equal to or greater than 1' do
+    @recipe.cooking_time = 0
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'description should be present' do
+    @recipe.description = nil
+    expect(@recipe).to_not be_valid
+  end
 end
