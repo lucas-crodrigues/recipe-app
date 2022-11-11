b148472a34715d756daf4977091034ecd7b9668f
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-11 20:52:27 -0300

    Create tests for users features and models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(name: 'Fernando', email: 'fernando.silvabr66@gmail.com', password: '123456', password_confirmation: '123456')
+  end
+
+  it 'name should be present' do
+    @user.name = nil
+    expect(@user).to_not be_valid
+  end
+
+  it 'email should be present' do
+    @user.email = nil
+    expect(@user).to_not be_valid
+  end
+
+  it 'password should be present' do
+    @user.password = nil
+    expect(@user).to_not be_valid
+  end
 end
